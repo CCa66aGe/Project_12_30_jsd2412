8e9602f1c5e5a9ce1f97baf0f6dd1b57516c1d5b
CCa66aGe <1640038836@qq.com>
2025-02-13 09:49:02 +0800

    11

--- src/reflect/ReflectDemo1.java
+++ src/reflect/ReflectDemo1.java
@@ -1,14 +1,30 @@
 package reflect;
 
+import java.lang.reflect.Method;
+
 /**
  *
  */
 public class ReflectDemo1 {
     public static void main(String[] args) {
         Class cls = String.class;
+        //通过类对象获取类的完全限定名
         String name = cls.getName();
         System.out.println(name);
+        //通过类对象获取简单名字
         name = cls.getSimpleName();
         System.out.println(name);
+
+        //通过类对象获取包对象
+        Package pack = cls.getPackage();
+        //通过包对象获取对应的包名
+        String packName = pack.getName();
+        System.out.println(packName);
+
+        Method[] methods = cls.getMethods();
+        for (Method method:methods){
+            System.out.println(method.getName());
+        }
+
     }
 }
