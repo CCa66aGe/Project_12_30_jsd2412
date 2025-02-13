1ecfe545b4498a24774470103f6ac9ac57260dbb
CCa66aGe <1640038836@qq.com>
2025-02-13 09:58:48 +0800

    11

--- src/reflect/ReflectDemo1.java
+++ src/reflect/ReflectDemo1.java
@@ -1,13 +1,20 @@
 package reflect;
 
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  *
  */
 public class ReflectDemo1 {
-    public static void main(String[] args) {
-        Class cls = String.class;
+    public static void main(String[] args) throws ClassNotFoundException {
+//        Class cls = String.class;
+//        Class cls = ArrayList.class;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入想要获取的类对象的完全限定名");
+        Class cls = Class.forName(sc.nextLine());
         //通过类对象获取类的完全限定名
         String name = cls.getName();
         System.out.println(name);
@@ -22,6 +29,7 @@ public class ReflectDemo1 {
         System.out.println(packName);
 
         Method[] methods = cls.getMethods();
+        System.out.println("一共有"+methods.length+"个方法");
         for (Method method:methods){
             System.out.println(method.getName());
         }
